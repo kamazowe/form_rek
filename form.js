343634815acc9b7301fd577bc9f97790f9ebde23
Kamil Gebala <kamazowe@gmail.com>
2018-02-16 13:14:18 +0100

    add keyPress event

--- form.js
+++ form.js
@@ -6,3 +6,15 @@ console.log(form)
 console.log(fields)
 
 
+//static
+
+fields.forEach((el)=>{
+   el.addEventListener('keypress',function(e){
+       e.preventDefault();
+        console.dir(e);
+        if(e.key === 'a'){
+            return e.returnValue = true;
+        }
+        return;
+   },false)
+})
